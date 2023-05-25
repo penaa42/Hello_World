486ee1228081bd54b1d87690fba251ebffe4431f
Adrian Pena <adrianpena383@gmail.com>
2023-05-25 17:35:29 -0400

    initial commit

---
+++ Button_clicker/script.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+function def_remove(element) {
+    element.remove();
+}
+
+function like_ninja(element) {
+    alert("Ninja was liked");
+}
