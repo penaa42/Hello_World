81b87c37ca16cba4c5f60528f4aa76f921019a1d
Adrian Pena <adrianpena383@gmail.com>
2023-05-28 17:00:04 -0400

    initial commit

---
+++ Making_Connections/script.js
@@ -0,0 +1,45 @@
+console.log("page loaded...");
+
+
+function change_profile() {
+    document.querySelector(".card-body h1").innerText = "Adrien D." 
+}
+
+
+function request_accept1() {
+    //console.log(`link`)
+    //console.log(document.querySelectorAll(".card-list-item")[0])
+    //let elements = document.getElementsByClassName(".card-list-item")
+    //console.log(elements[0])
+    //document.querySelector(".card-list-item").remove()
+//    li.card-list-item[0].parentNode.removeChild()
+
+}
+
+
+
+function request_accept3() {
+    //console.log(`link`)
+    console.log(document.querySelectorAll(".card-list-item")[1])
+    //let elements = document.getElementsByClassName(".card-list-item")
+    //console.log(elements[0])
+    document.querySelector(".card-list-item")[1].remove()
+//    li.card-list-item[0].parentNode.removeChild()
+}
+
+
+// function request_accept() {
+//     console.log(document.querySelector(".card-list-item"))
+//     console.log(document.querySelectorAll(".card-list-item")[0])
+//     //console.log(document.getElementsByTagName('li'))
+// }
+
+
+
+
+// function request_deny() {
+//     //console.log(`linked`)
+// }
+
+
+
