1b767ee7d82d2f2340316f04238056c3cca63296
Adrian Pena <adrianpena383@gmail.com>
2023-06-05 16:19:58 -0400

    set up an option to create a random pizza

--- Dojo_Pizzeria/Dojo_Pizzeria.js
+++ Dojo_Pizzeria/Dojo_Pizzeria.js
@@ -7,8 +7,6 @@
 // }
 // console.log(pizza);
 
-
-
 //creating multiple pizzas:
 function pizza_oven(crustType, sauceType, cheeses, toppings) {
     let pizza = {}
@@ -31,35 +29,18 @@ let pizza4 = pizza_oven("gluten free", "traditional", "mozzarella", ["bacon", "h
 //console.log(pizza4)
 
 
-
+// creating a random pizza
 function random_pizza(crustType, sauceType, cheeses, toppings){
     let pizza = {}
     pizza.crustType=   ["deep dish", "gluten free", "thin crust", "stuffed crist"],
     pizza.sauceType=   ["traditional", "buffalo", "bbq"],
     pizza.cheeses=   ["mozzarella", "feta", "provalone"],
-    pizza.toppings=    ["mushrooms", "olives", "onions", "pepperoni", "sausage", "bacon"]
-    
+    pizza.toppings=    ["mushrooms", "olives", "onions", "pepperoni", "sausage", "bacon"]    
     return pizza;
 }
 let rand_crust = random_pizza.pizza
 console.log(rand_crust)
 
-// let rand_sauce = Math.round(Math.random() * (pizza.sauceType.length -1))
-// console.log(rand_sauce)
-// let rand_cheese = Math.round(Math.random() * (pizza.cheeses.length -1))
-// console.log(rand_cheese)
-// let rand_topping = Math.round(Math.random() * (pizza.toppings.length -1))
-// console.log(rand_topping)
-
 console.log(random_pizza())
 //console.log(crustType.length)
-
-//console.log(Math.round(Math.random() * (pizza.crustType.length -1)))
-//console.log(Math.round(Math.random() * (pizza.crustType.length -1)))
-
-
-
-// let rand_sauce = Math.round(Math.random() * (snoopDog.weapons.length -1))
-// let rand_cheese = Math.round(Math.random() * (snoopDog.weapons.length -1))
-// let rand_topping = Math.round(Math.random() * (snoopDog.weapons.length -1))
 
